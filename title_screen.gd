af32d6f99f2b3d43160db33630e2d026fd6a724c
BofaNumber1 <jraahila@gmail.com>
2025-05-29 12:14:06 +1200

    Added script

---
+++ title_screen.gd
@@ -0,0 +1,23 @@
+extends Control
+
+
+
+func _ready():
+	pass
+
+
+
+func _process(delta):
+	pass
+
+
+func _on_start_pressed() -> void:
+	print("Start Pressed")
+
+
+func _on_setting_pressed() -> void:
+	print("Settings Pressed")
+
+
+func _on_quit_pressed() -> void:
+	print("Quit Pressed")
