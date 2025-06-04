4e7b44c15116c8039ef52f1828cd75b9110c5d97
BofaNumber1 <jraahila@gmail.com>
2025-06-05 09:22:13 +1200

    Reorginized Project Folders

---
+++ Scripts/title_screen.gd
@@ -0,0 +1,25 @@
+extends Control
+
+
+
+func _ready():
+	pass
+
+
+
+func _process(delta):
+	pass
+
+
+func _on_start_pressed() -> void:
+	Globals.next_scene = "res://Scenes/main_level.tscn"
+	get_tree().change_scene_to_packed(Globals.loading_screen)
+	print("Start Pressed")
+
+
+func _on_setting_pressed() -> void:
+	print("Settings Pressed")
+
+
+func _on_quit_pressed() -> void:
+	print("Quit Pressed")
--- globals.gd
+++
@@ -1,5 +0,0 @@
-extends Node
-
-var loading_screen = preload("res://loading_screen.tscn")
-
-var next_scene : String = "res://title_screen.tscn"
