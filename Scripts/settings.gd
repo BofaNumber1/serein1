c1fa9793f853c7b71d54941b5bad3d788ca8cccb
BofaNumber1 <jraahila@gmail.com>
2025-06-24 09:44:54 +1200

    REWORKED GUI
    New background, animated will work on buttons next time

--- Scripts/settings.gd
+++ Scripts/settings.gd
@@ -1,10 +1,21 @@
 extends Control
+var center : Vector2
+@onready var node = $Control
+
 
 func _ready():
 	$MarginContainer/VBoxContainer/Resolutions.clear()
 	$MarginContainer/VBoxContainer/Resolutions.add_item("1920 x 1080")
 	$MarginContainer/VBoxContainer/Resolutions.add_item("1600 x 900")
-	$MarginContainer/VBoxContainer/Resolutions.add_item("1280 x 720")	
+	$MarginContainer/VBoxContainer/Resolutions.add_item("1280 x 720")
+	center = Vector2(get_viewport_rect().size.x/2, get_viewport_rect().size.y/2)
+
+func _process(delta):
+	var tween = node.create_tween()
+	var offset = center - get_global_mouse_position() * 0.1
+	tween.tween_property(node, "position",offset,1.0)
+	if Input.is_action_just_pressed("ui_accept"):
+		$CPUParticles2D.emitting = !$CPUParticles2D.emitting
 
 
 func _on_volume_value_changed(value):
--- Scripts/title_screen.gd
+++ Scripts/title_screen.gd
@@ -1,13 +1,18 @@
 extends Control
 
-
+var center : Vector2
+@onready var node = $Control
 
 func _ready() -> void:
-	pass # Replace with function body.
+	# calculate center of the screen
+	center = Vector2(get_viewport_rect().size.x/2, get_viewport_rect().size.y/2)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	var tween = node.create_tween()
+	var offset = center - get_global_mouse_position() * 0.1
+	tween.tween_property(node, "position",offset,1.0)
 	if Input.is_action_just_pressed("ui_accept"):
 		$CPUParticles2D.emitting = !$CPUParticles2D.emitting
 
