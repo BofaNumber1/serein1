6959f7cff08ae69018588f03b55d7e1a44f01213
BofaNumber1 <jraahila@gmail.com>
2025-07-01 15:11:39 +1200

    Added intro

---
+++ Scripts/intro.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+@onready var animation_intro = $AnimationPlayer
+
+
+func _ready():
+	animation_intro.play("fade_in")
+	get_tree().create_timer(3).timeout.connect(fade_out)
+	
+
+func fade_out():
+	animation_intro.play("fade_out")
+	get_tree().create_timer(3).timeout.connect(title_screen)
+	
+
+func title_screen():
+	get_tree().change_scene_to_file("res://Scenes/title_screen.tscn")
