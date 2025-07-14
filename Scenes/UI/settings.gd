a32c918d9959721540cf00501c5271087ecd5f4d
BofaNumber1 <jraahila@gmail.com>
2025-07-14 14:23:54 +1200

    Revert "Added Settings menu"
    This reverts commit ba0533393c28de4ca9bb917e16fb2a0246debe57.

--- Scenes/UI/settings.gd
+++ Scenes/UI/settings.gd
@@ -8,11 +8,6 @@ class_name Settings
 
 
 func _on_exit_pressed() -> void:
-	$Click.play()
 	Globals.title_screen = "res://Scenes/UI/main_menu.tscn"
 	get_tree().change_scene_to_file("res://Scenes/UI/main_menu.tscn")
 	print("Back to menu Pressed")
-
-
-func _on_exit_mouse_entered() -> void:
-	$Hover.play()
