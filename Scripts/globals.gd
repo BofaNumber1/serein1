b6829bb92a71dfb6b21110f08de05d8a573fe2f7
Elijah-eshayBaa <elile87@kingshigh.school.nz>
2025-07-28 11:57:48 +1200

    New globals set
    Set to O-block from test sene

--- Scripts/globals.gd
+++ Scripts/globals.gd
@@ -3,7 +3,7 @@ extends Node
 var loading_screen = preload("res://Scenes/Loading Screen/3Dloading_screen.tscn")
 var settings_screen : String = "res://Scenes/UI/settings.tscn"
 var pause_settings : String = "res://Scenes/pause_settings.tscn"
-var next_scene : String = "res://Scenes/test_scene.tscn"
+var next_scene : String = "res://Scenes/Levels/O-Block.tscn"
 var title_screen : String = "res://Scenes/UI/main_menu.tscn"
 var pause_screen : String = "res://Scenes/pause_screen.tscn"
 var show_pause_menu: bool = false  # Flag to show pause menu after scene change
